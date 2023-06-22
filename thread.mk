ab073414a878bf1c639b96a8cc8d2738f59619cc
jiakang.cjk <jiakang.cjk@aliyun.com>
2023-06-22 14:06:32 +0800

    only windows use .exe

--- thread.mk
+++ thread.mk
@@ -2,7 +2,7 @@ CFLAGS = -O2 -Wall -pipe -pthread
 CXXFLAGS = ${CFLAGS}
 LDFLAGS =
 
-progs = client_thread.exe
+progs = client_thread$(EXT)
 
 all: ${progs}
 
@@ -10,5 +10,5 @@ queue.o: deque.o deque.h
 
 thread_pool_c.o: thread_pool_c.c thread_pool_c.h
 
-client_thread.exe: client_thread.o io_client.o io_common.o thread_pool_c.o deque.o
+client_thread$(EXT): client_thread.o io_client.o io_common.o thread_pool_c.o deque.o
 	${CC} ${CFLAGS} client_thread.o io_client.o io_common.o thread_pool_c.o deque.o ${LDFLAGS} -o $@
