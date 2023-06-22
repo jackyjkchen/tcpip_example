d78a2c3b5ca9c60e60e6bc35692ba97d91c9afe4
jiakang.cjk <jiakang.cjk@aliyun.com>
2023-06-22 14:22:55 +0800

    remove CXX and CXXFLAGS

--- thread.mk
+++ thread.mk
@@ -1,6 +1,4 @@
 CFLAGS = -O2 -Wall -pipe -pthread
-CXXFLAGS = ${CFLAGS}
-LDFLAGS =
 
 progs = client_thread$(EXT)
 
@@ -11,4 +9,4 @@ queue.o: deque.o deque.h
 thread_pool_c.o: thread_pool_c.c thread_pool_c.h
 
 client_thread$(EXT): client_thread.o io_client.o io_common.o thread_pool_c.o deque.o
-	${CC} ${CFLAGS} client_thread.o io_client.o io_common.o thread_pool_c.o deque.o ${LDFLAGS} -o $@
+	${CC} ${CFLAGS} $^ ${LDFLAGS} -o $@
